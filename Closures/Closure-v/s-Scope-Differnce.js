a854c2aa6ebcd73d3e24ee1698b2e41c42690334
unknown <amanyadav16544@gmail.com>
2024-03-16 21:26:18 +0530

    added Closure-Vs-Scope-Difference.js

---
+++ Closures/Closure-v/s-Scope-Differnce.js
@@ -0,0 +1,11 @@
+// Closures in JavaScript
+// Ques 10:- Differnce between Closure and Scope
+
+// Closure :-
+// WhenEver we create a function within another function then the inner function is the Closure
+// this closures is usally returned so we can use the outer function variable at a later type this is what a closure is 
+
+// where as a scope in Js:-
+// defines what variable we have acess too
+// there are two kinds of scope :- Global Scope & Local Scope,
+// and in case of closures there are :- Global scope , Outer scope and Local scope 
