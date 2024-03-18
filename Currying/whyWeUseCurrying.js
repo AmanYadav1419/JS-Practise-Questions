83f94d5d390d93c65cf5cf884c3313cc513e8358
unknown <amanyadav16544@gmail.com>
2024-03-18 19:18:33 +0530

    WhWeUseCurrying.js added

---
+++ Currying/whyWeUseCurrying.js
@@ -0,0 +1,10 @@
+// Currying in Javascript
+// Question :- Why Should we use Currying ?
+
+// Answers are given below:-
+
+// 1st point :- To avoid Passing the same variable again and again
+// 2nd point :- To create Higher order functions
+// 3rd point :- To make the function pure and less prone to errors
+
+// only theory part is there "Jay Siya Ram 🕉🔱"
