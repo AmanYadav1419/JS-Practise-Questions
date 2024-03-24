d722341e1a2e20b42d760e7a74c9ead54137612c
unknown <amanyadav16544@gmail.com>
2024-03-24 10:30:56 +0530

    added & completed question containsDuplicate.js

--- DSA-LeetcodeQuestions/containsDuplicate.js
+++ DSA-LeetcodeQuestions/containsDuplicate.js
@@ -11,3 +11,26 @@
 // Example 3:- 
 // Input num :- [1,2,3,4,1,2,4,3,1]
 // output :- true
+
+let checkDuplicateOrNot = function(nums){
+    let uniqueSet = new Set();
+
+    for(let num of nums){
+        if(uniqueSet.has(num)){
+            return true
+        }
+
+        uniqueSet.add(num);
+    }
+    return false
+}
+
+// test Cases
+
+const num1 = [1,2,3,1]
+const num2 = [1,2,3,4]
+const num3 = [1,2,3,4,1,2,4,3,1];
+
+console.log(checkDuplicateOrNot(num1)); //it will return true
+console.log(checkDuplicateOrNot(num2)) //it will return false
+console.log(checkDuplicateOrNot(num3)) //it will return true
